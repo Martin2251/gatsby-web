2d7becc9b30a1f6d626d1ad26a15a45114ddf6c4
martin lowe <martinlowe673@gmail.com>
2020-11-03 12:55:23 +0000

    new website

--- src/mock/data.js
+++ src/mock/data.js
@@ -2,64 +2,56 @@ import { nanoid } from 'nanoid';
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
+  title: 'Martin Lowe', // e.g: 'Name | Developer'
   lang: '', // e.g: en, es, fr, jp
-  description: '', // e.g: Welcome to my website
+  description: 'Welcome to my website', // e.g: Welcome to my website
 };
 
 // HERO DATA
 export const heroData = {
   title: '',
-  name: '',
-  subtitle: '',
+  name: 'Martin Lowe',
+  subtitle: 'Full stack Web Developer',
   cta: '',
 };
 
 // ABOUT DATA
 export const aboutData = {
   img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  paragraphOne: 'I have recently completed the Le Wagon Full Stack Web Development bootcamp and looking for my fist role as a developer',
+  paragraphTwo: 'I have 5 years previous experience in various sales and accout management roles in Public Private and Not for Profit ',
+  paragraphThree: 'I play rugby for the Kings Cross Steelers the first LGBT Rugby club which I also take part in orgainising club tours, I also part time DJ',
+  resume: 'https://www.canva.com/design/DAEFHhc76DY/8Fj7AuC7dyq5nFSp8nnFJA/view?utm_content=DAEFHhc76DY&utm_campaign=designshare&utm_medium=link&utm_source=sharebutton', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
 export const projectsData = [
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
+    img: 'Project1.png',
+    title: 'BuyBaby',
+    info: 'A website which I was involved in using Ruby on Rails, Javascript, HTML & CSS, it has 3 user journeys Buy, Sell and Swap Baby Clothing and Accessories',
     info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    url: 'https://www.buybaby.shop/',
+    repo: 'https://github.com/anniebartlett/BuyBaby', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
+    img: 'Project2.png',
+    title: 'Plantastic',
+    info: 'A website which I help build following the MVC pattern with Ruby on Rails, the website is based on 1 user journeys which was renting plants',
     info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    url: 'https://plantastic-martin2251.herokuapp.com/',
+    repo: 'https://github.com/Martin2251/Plantastic', // if no repo, the button will not show up
   },
-  {
-    id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
-  },
+
 ];
 
 // CONTACT DATA
 export const contactData = {
   cta: '',
   btn: '',
-  email: '',
+  email: 'martinlowe673@gmail.com',
 };
 
 // FOOTER DATA
@@ -67,28 +59,18 @@ export const footerData = {
   networks: [
     {
       id: nanoid(),
-      name: 'twitter',
-      url: '',
-    },
-    {
-      id: nanoid(),
-      name: 'codepen',
-      url: '',
-    },
-    {
-      id: nanoid(),
       name: 'linkedin',
-      url: '',
+      url: 'https://www.linkedin.com/in/martin-lowe321/',
     },
     {
       id: nanoid(),
       name: 'github',
-      url: '',
+      url: 'https://github.com/Martin2251',
     },
   ],
 };
 
 // Github start/fork buttons
 export const githubButtons = {
-  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
+  isEnabled: false, // set to false to disable the GitHub stars/fork buttons
 };
